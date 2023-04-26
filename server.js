d107166e4e8ae3bd55bad56dafd4d0483525ae4b
Celeste A. Moore <77650238+celestealexmoore@users.noreply.github.com>
2023-04-26 17:41:35 -0500

    removed ejs and used 'path' per requirements.

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require("express");
+path = require("path");
 const PORT = 3001;
 const app = express();
 
-app.set("view engine", "ejs");
-
-// middleware
-
-// routes
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/notes", (req, res) => {
-    res.render("notes");
-  });
+// html routes
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}!`);
